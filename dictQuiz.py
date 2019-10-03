53947c24f07a490ccffaaff0893395f563c81ff1
jackrb <jackrichardbollans@gmail.com>
2019-10-03 22:15:14 +0300

    First commit to quiz branch development

---
+++ dictQuiz.py
@@ -0,0 +1,4 @@
+#Create game test here.
+#Use the count number on the spreadhseet to determine which words the user has recently been reminded of and tests those with least recalls.
+#USer can flag if they don't remeber a word by. Some logic for this needs to be devised.
+ 
